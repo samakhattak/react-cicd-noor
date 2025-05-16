e96f60408b05dab321c871cfcd47c6459626002f
Noor Us Sama Tariq <168010773+samakhattak@users.noreply.github.com>
2025-05-16 11:33:33 +0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello from Noor's CI/CD Lab 🎯</h1>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
